f3550d297f08357317ce4104bd9d46e3d2738a6d
GaffarovDaniel <daniel.gaffarov@mail.ru>
2023-09-30 09:28:59 +0300

    Добавление всех файлов

---
+++ number_one/Program.cs
@@ -0,0 +1,14 @@
+﻿static void PrintNumbersFromNto1(int N)
+{
+    if (N >= 1)
+    {
+        Console.Write(N);
+            if (N > 1)
+                Console.Write(", ");
+        PrintNumbersFromNto1(N - 1);
+    }
+}
+
+Console.WriteLine("");
+int N = Convert.ToInt32(Console.ReadLine()); // Замените значение N на нужное
+PrintNumbersFromNto1(N);
---
+++ number_two/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Введите M");
+int M = Convert.ToInt32(Console.ReadLine()); // Замените значение M на нужное
+System.Console.WriteLine("Введите N");
+int N = Convert.ToInt32(Console.ReadLine()); // Замените значение N на нужное
+
+int sum = 0;
+
+for (int i = M; i <= N; i++)
+{
+    sum += i;
+}
+
+ Console.WriteLine("Сумма натуральных чисел от " + M + " до " + N + " равна: " + sum);
